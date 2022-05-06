0fc45d81b5dc661a9c03e7832a159e6781b13e8f
Christopher Hahn <c.hahn99@web.de>
2022-05-06 12:56:00 -0500

    MLP experiments

--- ppo/nn.py
+++ ppo/nn.py
@@ -5,8 +5,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 
-from spinup.algos.pytorch.ppo.core import Actor
-
 class ConvNet(nn.Module):
     class Block(nn.Module):
         def __init__(self, n_input, n_output, kernel_size=3, stride=1):
@@ -48,6 +46,27 @@ class ConvNet(nn.Module):
         z = self.classifier(z.mean(dim=[1, 2]))
 
         return z
+
+# straight up compied from spinup.algos.pytorch.ppo.core to not depend on the library in this file
+# since there are version conflicts...
+# Rendering the environment does not work in a conda env with spinup since spinup depends on TensorFlow 1.x which is only available in Python <= 3.6
+# however there is a bug that happens when trying to render on newer versions of Mac OS which was only fixed in Python > 3.8...
+class Actor(nn.Module):
+    def _distribution(self, obs):
+        raise NotImplementedError
+
+    def _log_prob_from_distribution(self, pi, act):
+        raise NotImplementedError
+
+    def forward(self, obs, act=None):
+        # Produce action distributions for given observations, and
+        # optionally compute the log likelihood of given actions under
+        # those distributions.
+        pi = self._distribution(obs)
+        logp_a = None
+        if act is not None:
+            logp_a = self._log_prob_from_distribution(pi, act)
+        return pi, logp_a
 
 # CNN Critic; adapted from MLPCritic (spinup.algos.pytorch.ppo.core)
 class CNNCritic(nn.Module):
@@ -91,6 +110,62 @@ class CNNActorCritic(nn.Module):
             logp_a = self.pi._log_prob_from_distribution(pi, a)
             v = self.v(obs)
         return a.item(), v.numpy(), logp_a.numpy()
+
+    def act(self, obs):
+        return self.step(obs)[0]
+
+
+# below also adapted from core file to avoid dependency on the spinup import... (reasons in line 50)
+# also to adapt to our other network formats
+
+def mlp(sizes, activation, output_activation=nn.Identity):
+    layers = []
+    for j in range(len(sizes)-1):
+        act = activation if j < len(sizes)-2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
+    return nn.Sequential(*layers)
+
+class MLPCategoricalActor(Actor):
+    def __init__(self, obs_dim, act_dim, hidden_sizes):
+        super().__init__()
+        self.logits_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], nn.ReLU)
+
+    def _distribution(self, obs):
+        logits = self.logits_net(obs)
+        return Categorical(logits=logits)
+
+    def _log_prob_from_distribution(self, pi, act):
+        return pi.log_prob(act)
+
+
+class MLPCritic(nn.Module):
+    def __init__(self, obs_dim, hidden_sizes):
+        super().__init__()
+        self.v_net = mlp([obs_dim] + list(hidden_sizes) + [1], nn.ReLU)
+
+    def forward(self, obs):
+        return torch.squeeze(self.v_net(obs), -1) # Critical to ensure v has right shape.
+
+
+
+class MLPActorCritic(nn.Module):
+    def __init__(self, observation_space, action_space, layers):
+        super().__init__()
+
+        obs_dim = observation_space.shape[0]
+
+        self.pi = MLPCategoricalActor(obs_dim, action_space.n, layers)
+
+        # build value function
+        self.v  = MLPCritic(obs_dim, layers)
+
+    def step(self, obs):
+        with torch.no_grad():
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v = self.v(obs)
+        return a.numpy(), v.numpy(), logp_a.numpy()
 
     def act(self, obs):
         return self.step(obs)[0]
--- ppo/ppo.py
+++ ppo/ppo.py
@@ -10,96 +10,8 @@ from spinup.utils.run_utils import ExperimentGrid
 import gym
 from gym_puyopuyo import register
 
-from nn import CNNActorCritic
-
-# wrapper to process state, make it ready for CNN
-class ProcessStateCNN(gym.ObservationWrapper):
-    def __init__(self, env=None, consider_future=False):
-        super(ProcessStateCNN, self).__init__(env)
-        self.env = env
-
-        orig_shape = env.observation_space.spaces[1].shape
-
-        self.consider_future = consider_future
-
-        if consider_future:
-            self.observation_space = gym.spaces.Box(low=0, high=1, shape=(7, orig_shape[1], orig_shape[2]), dtype=int)
-        else:
-            self.observation_space = gym.spaces.Box(low=0, high=1, shape=(3, orig_shape[1], orig_shape[2]), dtype=int)
-
-    def observation(self, obs):
-        '''
-        Take game field and piece data and encode combination.
-        This is supposed to solve the issue of multiple possible game states being equivalent; it does not really matter if a
-        field is blue or red; the relation between the field and the other fields and the falling game pieces is what matters.
-        So we're encoding a matric of size (3 (or 7 if considering future pieces), field_height, field_width) the following way:
-        first channel: 1 for every field that matches color of left piece of current puyo
-        second channel: 1 for every field that matches color of right piece of current puyo
-        (if considering future pieces: third-sixth channel based on future pieces instead of the current one)
-        third channel: 1 for every field that is not empty
-        '''
-
-        # get field data, piece data
-        field_data = obs[1]
-        pieces_data = obs[0]
-
-        # transpose for simplicity
-        field_data = field_data.transpose([1,2,0])
-        pieces_data = pieces_data.transpose([1,2,0])
-
-        # find out where fields don't have color
-        no_color = np.expand_dims((field_data.sum(axis=-1) == 0).astype(int), axis=-1)
-        # inverse
-        filled = (no_color == 0).astype(int)
-
-        # combine arrays (no color is now a new color)
-        field_arr = np.concatenate((field_data, no_color), axis=-1)
-
-        # turn into indices instead of one-hot
-        argmax_arr = np.argmax(field_arr, axis = -1)
-        argmax_color = np.argmax(pieces_data, axis = -1)
-
-
-        channels_list = []
-
-        if self.consider_future:
-            for i in range(argmax_color.shape[0]):
-                for j in range(argmax_color[0].shape[0]):
-                    channels_list.append(np.expand_dims((argmax_arr == argmax_color[i][j]).astype(int), axis=-1))
-        else:
-            for i in range(argmax_color[0].shape[0]):
-                channels_list.append(np.expand_dims((argmax_arr == argmax_color[0][i]).astype(int), axis=-1))
-
-        channels_list.append(filled)
-
-        # combine all, transpose into original (colors, height, width) again
-        return np.expand_dims(np.stack(channels_list, axis=-1).squeeze().transpose([2,0,1]), axis=0)
-
-
-# wrapper to process state, make it ready for MLP network
-class ProcessStateMLP(gym.ObservationWrapper):
-    def __init__(self, env=None):
-        super(ProcessStateMLP, self).__init__(env)
-        self.env = env
-
-        orig_shape = env.observation_space.spaces[1].shape
-
-        # calculate size of flattened observation
-        self.observation_space = 1
-
-        # multiply dimensions of game field
-        for dim in orig_shape:
-            self.observation_space *= dim
-
-        # add size of future puyo stack
-        self.observation_space += 3 * 6 * 2
-
-
-    def observation(self, obs):
-        '''
-        simply concatenate flattened game field, future puyos
-        '''
-        return np.concatenate(obs[0].flatten(), obs[1].flatten())
+from nn import CNNActorCritic, MLPActorCritic
+from process_state import ProcessStateCNN, ProcessStateMLP
 
 def test_nn(consider_future, small, cnn, gamma):
     register()
@@ -133,7 +45,7 @@ def test_nn(consider_future, small, cnn, gamma):
     else:
         raise NotImplementedError
 
-def create_env_wide():
+def create_env_wide_cnn():
     from gym_puyopuyo import register
 
     register()
@@ -142,7 +54,7 @@ def create_env_wide():
     env = ProcessStateCNN(env, False)
     return env
 
-def create_env_wide_future():
+def create_env_wide_future_cnn():
     from gym_puyopuyo import register
 
     register()
@@ -151,7 +63,7 @@ def create_env_wide_future():
     env = ProcessStateCNN(env, True)
     return env
 
-def create_env_small():
+def create_env_small_cnn():
     from gym_puyopuyo import register
 
     register()
@@ -160,13 +72,31 @@ def create_env_small():
     env = ProcessStateCNN(env, False)
     return env
 
-def create_env_small_future():
+def create_env_small_future_cnn():
     from gym_puyopuyo import register
 
     register()
 
     env = gym.make("PuyoPuyoEndlessSmall-v2")
     env = ProcessStateCNN(env, True)
+    return env
+
+def create_env_wide_mlp():
+    from gym_puyopuyo import register
+
+    register()
+
+    env = gym.make("PuyoPuyoEndlessWide-v2")
+    env = ProcessStateMLP(env)
+    return env
+
+def create_env_small_mlp():
+    from gym_puyopuyo import register
+
+    register()
+
+    env = gym.make("PuyoPuyoEndlessSmall-v2")
+    env = ProcessStateMLP(env)
     return env
 
 if __name__ == "__main__":
@@ -182,13 +112,13 @@ if __name__ == "__main__":
     '''
 
     eg = ExperimentGrid(name='ppo-pyt-bench')
-    eg.add('env_fn', [create_env_small, create_env_small_future], 'env_fn')
-    eg.add('actor_critic', CNNActorCritic)
+    eg.add('env_fn', [create_env_small_mlp, create_env_wide_mlp], 'env_fn')
+    eg.add('actor_critic', MLPActorCritic)
     eg.add('gamma', [0.9,0.95,0.98,0.99,0.999], 'gamma')
     eg.add('epochs', 150)
     eg.add('steps_per_epoch', 4000)
     eg.add('max_ep_len', 100)
-    eg.add('ac_kwargs:layers', [[4], [8], [8,16], [8,16,16], [32,64,64]], 'layers')
+    eg.add('ac_kwargs:layers', [[], [16], [64], [32, 16], [128, 64]], 'layers')
     eg.run(ppo, num_cpu=6)
 
 
